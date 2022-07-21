bdd093f23cf93bddac35c0e1f5c429294fb04d5b
JESKA JENIPHER <jeska.jenipher@student.moringaschool.com>
2022-07-21 10:31:30 +0300

    add App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,19 @@
 // create your App component here
+import React, { useState, useEffect } from 'react'
+
+export default function App() {
+  const [imgSrc, setImgSrc] = useState(null);
+
+  useEffect(() => {
+    const URL = "https://dog.ceo/api/breeds/image/random"
+    fetch(URL)
+      .then(r => r.json())
+      .then(data => setImgSrc(data.message))
+  }, [setImgSrc])
+
+  if (imgSrc === null) {
+    return <p>Loading...</p>
+  }  
+
+  return <img src={imgSrc} alt="A Random Dog" />
+}
